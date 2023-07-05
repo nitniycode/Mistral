451f03dfbcdc36d26f1775c3ca6803f8db1157f5
nitin yadav <ny941078@gmail.com>
2023-07-05 13:32:04 +0530

    created logo, installed react router dom and some pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Header from './components/Header';
+import Home from './pages/Home'
+import Products from './pages/Products'
+import New from './pages/New'
+import Ourstory from './pages/Ourstory'
+import Contact from './pages/Contact'
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img  className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/new' element={<New />} />
+        <Route path='/ourstory' element={<Ourstory />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+     </Router> 
+    </>
   );
 }
 
